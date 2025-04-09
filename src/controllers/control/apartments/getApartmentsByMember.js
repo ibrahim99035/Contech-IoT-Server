5ea6b64e517874cb9518859f27b7acc9852af0a7
ibrahim <ebrahimaboeita990@gmail.com>
2025-04-09 17:11:37 +0200

    object of objects

--- src/controllers/control/apartments/getApartmentsByMember.js
+++ src/controllers/control/apartments/getApartmentsByMember.js
@@ -9,23 +9,29 @@ exports.getApartmentsByMember = async (req, res) => {
     const userId = req.user._id;
     
     // Find apartments where user is either a member OR creator
-    const apartments = await Apartment.find({
+    const apartmentsArray = await Apartment.find({
       $or: [
         { members: userId },
         { creator: userId }
       ]
     })
-    .populate('creator', 'name email role') // Added role for creator
-    .populate('members', 'name email role') // Added role for members
+    .populate('creator', 'name email role')
+    .populate('members', 'name email role')
     .populate('rooms', 'name')
     .select('name creator members rooms')
     .lean();
     
+    // Convert array to object of objects using _id as keys
+    const apartmentsObject = {};
+    apartmentsArray.forEach(apartment => {
+      apartmentsObject[apartment._id] = apartment;
+    });
+    
     // Debugging info
     console.log('User ID:', userId);
-    console.log('Found apartments:', apartments.length);
+    console.log('Found apartments:', Object.keys(apartmentsObject).length);
     
-    res.json(apartments);
+    res.json(apartmentsObject);
   } catch (error) {
     console.error('Error fetching apartments:', error);
     res.status(500).json({ message: 'Error fetching apartments for the user', error: error.message });
